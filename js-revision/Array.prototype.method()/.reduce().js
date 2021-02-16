fdb6d95951b5dd377f5d20cee7c1ca85bf731f49
gustavdias <gustav.almd@gmail.com>
2021-02-16 11:28:43 +0300

    add redux, redux-thunk, react-redux

---
+++ js-revision/Array.prototype.method()/.reduce().js
@@ -0,0 +1,25 @@
+//Array reduce method
+//reduce all the values in an array into a single result
+// Uses a callback function just like map, forEach, filter, etc
+// array.reduce(callback, initialValue)
+// also has a second parameter which is an initialValue
+
+
+let numbers = [12, 34, 56, 78, 91];
+//find the sum of all the numbers
+
+let movies = ['Layer Cake', 'Star Wars', 'Star Trek', 'Jaws', 'Jurassic Park', 'Gross Pointe Blank', 'Eternal Sunshine of the Spotless Mind', 'Memento', 'Dog Soldiers', 'The Host', 'Gran Torino', 'Close Encounters of the Third Kind', 'Good Will Hunting', 'Casino Royale', 'Almost Famous'];
+//find the first movie alphabetically
+
+
+let sum = numbers.reduce(function(passedIn, item){
+    //console.log(passedIn, item);
+    return passedIn + item;
+}, 0);
+console.log('Total is', sum, '\n');
+
+let first = movies.reduce(function(current, item){
+    console.log('comparing', current, 'to', item);
+    return (current < item) ? current: item;
+}, "\u0434");
+console.log('First movie is', first);
